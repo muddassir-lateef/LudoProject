f542431f1d2a2b555fb369c4d495a49a47907f12
Muddassir Lateef <lateef.muddassir@gmail.com>
2021-06-09 21:09:28 +0500

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <time.h>
+using namespace sf;
+
+Vector2i offset(48,24);
+
+
+int main()
+{
+
+    RenderWindow app(VideoMode(1280,720), "Ludo");
+    app.setFramerateLimit(60);
+    sf::Music music;
+    if (!music.openFromFile("audio/mainmenu/strongandstrike.wav"))
+      return -1; // error
+    music.play();
+    music.setVolume(50);
+    Texture t1;
+    t1.loadFromFile("images/mainmenu/menuboard.jpg");
+    Sprite background(t1);
+    while (app.isOpen())
+    {
+      Event e;
+      while (app.pollEvent(e))
+      {
+          if (e.type == Event::Closed)
+            app.close();
+                   
+          if (e.type == Event::MouseButtonPressed)
+            if (e.key.code == Mouse::Left)
+            {
+
+            }
+      }
+
+      app.draw(background);
+      app.display();
+    }
+    return 0;
+}
