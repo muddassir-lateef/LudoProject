1515f1599f1f6e26618f2594909130cfb7b2bd03
Muddassir Lateef <lateef.muddassir@gmail.com>
2021-06-09 21:40:55 +0500

    comment 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <time.h>
+#include<iostream>
+
 using namespace sf;
 
 Vector2i offset(48,24);
@@ -8,7 +10,7 @@ Vector2i offset(48,24);
 
 int main()
 {
-
+    std::cout<<"Muddassir"<<std::endl;
     RenderWindow app(VideoMode(1280,720), "Ludo",sf::Style::Fullscreen);
     app.setFramerateLimit(60);//asdf
     sf::Music music;
