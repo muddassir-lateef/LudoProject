46eb2c393928b598458099fd171552b4c5945c43
Shayan Zuberi <i190749@nu.edu.pk>
2021-06-11 17:52:02 +0500

    roll animation - commit 1

---
+++ src/dice.h
@@ -0,0 +1,107 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <time.h>
+#include <vector>
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class dice{
+    int x;
+    int y;
+    Sprite Dice;
+    SoundBuffer sound;
+    int opacity;
+    bool rolling;
+    int face=6;
+public:
+    dice(int x, int y);
+    dice(int x, int y, Texture& texture);
+    void drawDice(int face, RenderWindow& app); //only for drawing dice of any face Value at given coordinates    
+    vector<int> roll_Dice(int type, RenderWindow& app);
+    void animate_roll(int result, RenderWindow& app);
+    void setTexture(Texture& texture);
+    Sprite& getDice();
+    void setx(int x);
+    void sety(int y);
+    bool isRolling();
+};
+
+dice::dice(int x, int y){
+    this->x = x;
+    this->y = y;
+    opacity = 255;
+    Dice.setPosition(this->x,this->y);
+    rolling = false;
+    sound.loadFromFile("audio/Dice_Roll/dice_roll.wav");;
+}
+
+dice::dice(int x, int y, Texture& texture){
+    this->x = x;
+    this->y = y;
+    opacity = 255;
+    rolling = false;
+    Dice.setPosition(this->x,this->y);
+    Dice.setTexture(texture);
+    sound.loadFromFile("audio/Dice_Roll/dice_roll.wav");
+}
+
+bool dice::isRolling(){
+    return rolling;
+}
+
+Sprite& dice::getDice(){
+    return Dice;
+}
+
+void dice::setx(int x){
+    this->x = x;
+}
+void dice::sety(int y){
+    this->y = y;
+}
+
+void dice::setTexture(Texture& texture){
+    this->Dice.setTexture(texture);
+}
+
+void dice::drawDice(int faceValue, RenderWindow& app){  //only for drawing dice of any face Value at given coordinates
+  
+  if (!rolling) faceValue = face;
+  if (faceValue < 0 || faceValue > 6) return;
+  IntRect obj((faceValue-1)*50,0,50,50);
+  Dice.setTextureRect(obj);
+  app.draw(Dice);
+}
+
+vector<int> dice::roll_Dice(int type, RenderWindow& app){
+  rolling = true;
+  Sound rolling_sound;
+  rolling_sound.setBuffer(sound);
+  rolling_sound.setVolume(100);
+  rolling_sound.play();
+  srand(time(0)); //seeding
+  vector<int> roll_values;  //return list 
+  int value = (rand()%6)+1;
+  cout<<value<<endl;
+  face = value;
+  roll_values.push_back(value); //adding to the list of generated values
+  //animate_roll(value, app);  //animating roll with the value
+  return roll_values;
+}
+
+void dice::animate_roll(int result, RenderWindow& app){
+  if (rolling == true){ 
+    Clock clock;
+    clock.restart();
+    opacity -= 10;
+    Dice.setColor(Color(255,255,255,opacity));
+    Dice.setScale(1.01f,1.01f);
+    if (opacity < 0){
+        rolling = false;
+        opacity = 255;
+        Dice.setScale(1.00f,1.00f);
+        this->drawDice(face,app);
+    }
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,31 @@
 #include <SFML/Audio.hpp>
 #include <time.h>
 #include<iostream>
-#include <vector>
 #include <ctime>
+#include<unistd.h>
+#include "dice.h"
 using namespace sf;
 using namespace std;
-
-Vector2i offset(48,24);
-vector<int> roll_Dice(int type, Sprite& dice, RenderWindow& app);  //roll dice functionality
-void animate_roll(int result, Sprite& dice, RenderWindow& app); //animate single role with showing result at the end 
-void drawDice(int x, int y, Sprite& dice, int face, RenderWindow& app);  //draw dice at given coordinates
-int dice_x = 750;
-int dice_y = 650;
+int x=1;
 
 int main()
 {
-    std::cout<<"Changes in Dice roll\n";
-    std::cout<<"Hello world by Shayan\n";
     RenderWindow app(VideoMode(1280,720), "Ludo");
     app.setFramerateLimit(60);
     sf::Music music;
     if (!music.openFromFile("audio/mainmenu/strongandstrike.wav"))
       return -1; // error
-    music.play();
-    music.setVolume(50);
+    //music.play();
+    music.setVolume(20);
     Texture bg;
     bg.loadFromFile("images/background/board.png");
     Sprite background(bg);
     Texture dice_texture;
-    dice_texture.loadFromFile("images/dice/dices3.png");
-    Sprite dice(dice_texture);
+    dice_texture.loadFromFile("images/dice/dices6.png");
+    dice Dice(750,600,dice_texture);
     while (app.isOpen())
     {
+      app.clear();
       Event e;
       while (app.pollEvent(e))
       {
@@ -42,44 +36,22 @@ int main()
           if (e.type == Event::MouseButtonPressed)
             if (e.key.code == Mouse::Left)
             {
-
+              Dice.roll_Dice(123,app);
             }
           if (e.type == Event::KeyPressed){
             if(e.key.code == Keyboard::Escape)
               app.close();
           }  
       }
-      //drawDice(dice_x,dice_y,6,app);
-      animate_roll(6, dice, app);
-
+      if (Dice.isRolling()) x = (rand()%6)+1; //give a new x to display
+      Dice.animate_roll(x,app);
+      Dice.drawDice(x,app);
       app.draw(background);
       app.display();
     }
     return 0;
 }
 
-void drawDice(int x, int y, Sprite& dice, int face, RenderWindow& app){  //only for drawing dice of any face Value at given coordinates
-  if (face < 0 || face > 6) return;
-  dice.setPosition(x,y);
-  if (face<4)
-    dice.setTextureRect(IntRect((face-1)*50,0,50,50));  //each dice is 50x50
-  else
-    dice.setTextureRect(IntRect((face-4)*50,50,50,50));
-  app.draw(dice);
-}
 
-void animate_roll(int result, Sprite& dice, RenderWindow& app){
-  for (int i=1; i<7; i++){
-    drawDice(dice_x, dice_y, dice, i, app);
-  }
-  drawDice(dice_x, dice_y, dice, result, app);
-}
 
-vector<int> roll_Dice(int type, Sprite& dice, RenderWindow& app){
-  srand(time(0)); //seeding
-  vector<int> roll_values;  //return list 
-  int value = (rand()%6)+1;
-  roll_values.push_back(value); //adding to the list of generated values
-  animate_roll(value, dice, app);  //animating roll with the value
-  return roll_values;
-}
+
