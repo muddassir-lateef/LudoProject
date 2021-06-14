43689febbb705b7e383e2473b2021c84cbc53b4e
Muddassir Lateef <lateef.muddassir@gmail.com>
2021-06-14 05:20:30 +0500

    Board Structure with mapping and constraints

--- main.d
+++ main.d
@@ -1,2 +1,2 @@
 obj/main.o: src/main.cpp src/../classes/Board.h src/../classes/./Token.h \
- src/../classes/Token.h
+ src/../classes/../Utilities/strutils.cpp src/../classes/Token.h
--- src/main.cpp
+++ src/main.cpp
@@ -30,15 +30,12 @@ int main()
     //Making sprites from textures
     Sprite boardS(t[0]);
     Sprite tokensS[4]={Sprite(t[1]),Sprite(t[2]),Sprite(t[3]),Sprite(t[4])};
-        std::cout<<"got here0"<<std::endl;
-
     //initializing classes with sprites
     Board board(0,0,&boardS,&app);
     board.initTokens(tokensS);
-    std::cout<<"got here"<<std::endl;
+   // int nx=0;
     while (app.isOpen())
     {
-           // board.draw();
       Event e;
       while (app.pollEvent(e))
       {
@@ -48,22 +45,26 @@ int main()
           if (e.type == Event::MouseButtonPressed)
             if (e.key.code == Mouse::Left)
             {
-
+                std::cout<<"x:"<<Mouse::getPosition().x<<" "<<"y:"<<Mouse::getPosition().y<<std::endl;
             }
-          if (e.type == Event::KeyPressed){
+          if (e.type == Event::KeyPressed)
+          {
             if(e.key.code == Keyboard::Escape)
               app.close();
-            if(e.key.code == Keyboard::Down)
-              board.movet(0,1);
-            if(e.key.code == Keyboard::Up)
-              board.movet(0,-1);
-            if(e.key.code == Keyboard::Left)
-              board.movet(-1,0);
-            if(e.key.code == Keyboard::Right)
-              board.movet(1,0);
+            if(e.key.code == Keyboard::F)
+            {
+              //nx++;
+             // board.move(1,8,13,nx);
+            //  board.move(2,1,8,nx);
+              //board.move(3,6,1,nx);
+             // board.move(4,13,6,nx);
+
+            }
+
               
           }  
       }
+      board.draw();
       app.display();
     }
     return 0;
